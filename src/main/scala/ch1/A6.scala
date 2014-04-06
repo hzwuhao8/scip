dd33d7d9542001c0b4c8a86760782f5f2d348ea8
wuhao <hzwuhao8@gmail.com>
2014-04-06 22:26:52 +0800

    newton method 抽象

--- src/main/scala/ch1/A6.scala
+++ src/main/scala/ch1/A6.scala
@@ -1,6 +1,9 @@
 package ch1
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 object A6 extends App {
+  val log = LoggerFactory.getLogger(A6.getClass())
 
   def newif(p: Boolean, f1: => Double, f2: => Double): Double = {
     p match {
@@ -9,32 +12,48 @@ object A6 extends App {
     }
   }
 
-  def sqrt(x: Double): Double = {
+  def newton(x: Double,   improve: Double => Double): Double = {
 
     def good(guess: Double, nextguess: Double): Boolean = {
-      Math.abs(guess - nextguess) <  1e-15
+      Math.abs(guess - nextguess) / guess < 1e-15
     }
-
-    def improve(guess: Double): Double = {
-      average(guess, x / guess)
-    }
-
-    def average(x: Double, y: Double) = {
-      (x + y) / 2
-    }
-
-    def sqrtIter(guess: Double): Double = {
+    
+    def newtonInter(guess: Double): Double = {
       val nextguess = improve(guess);
+
       if (good(guess, nextguess)) {
-        guess
+        (guess + nextguess) / 2
       } else {
-        sqrtIter(nextguess)
+        newtonInter(nextguess)
       }
     }
-
-    sqrtIter(1.0)
+    
+    newtonInter(1.0)
   }
 
-  println( sqrt( 0.2 ))
-  println( Math.sqrt( 0.2))
+  def sqrt(x: Double): Double = {
+
+    def improve(guess: Double): Double = {
+      def average(x: Double, y: Double) = {
+        (x + y) / 2
+      }
+      average(guess, x / guess)
+    }
+    newton(x, improve)
+
+  }
+  
+  def cube(x: Double): Double = {
+     def improve(guess: Double): Double = {
+      (( x / (guess*guess)) + 2*guess)/3
+       
+    }
+    newton(x, improve)
+  }
+  val x = 27
+  log.info("sqrt( {} )={} ", x, sqrt(x))
+  log.info("Math.sqrt( {} )={}", x, Math.sqrt(x))
+  
+ log.info("cube( {} )={} ", x,   (cube(x)) )
+  log.info("Math.pow( {} )={}", x, Math.pow(x , 1/3.0) )
 }
